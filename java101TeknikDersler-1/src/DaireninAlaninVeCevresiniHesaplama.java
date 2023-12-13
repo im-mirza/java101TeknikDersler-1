14696a4dbbfee34c4394efe11bc39fb9b5491e40
Mirza <mirza.ismayilov@bil.omu.edu.tr>
2023-12-13 10:18:56 +0300

    degismedim

---
+++ java101TeknikDersler-1/src/DaireninAlaninVeCevresiniHesaplama.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DaireninAlaninVeCevresiniHesaplama {
+    public static void main(String[] args) {
+        int r;
+        final double pi=3.14;
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Dairenin Yari Capini Giriniz:");
+        r=sc.nextInt();
+
+        double alan =pi*r*r;
+        double cevre=2*pi*r;
+
+        System.out.println("Dairenin alani: "+alan);
+        System.out.println("Dairenin Cevresi: "+cevre); 
+
+    }
+}
