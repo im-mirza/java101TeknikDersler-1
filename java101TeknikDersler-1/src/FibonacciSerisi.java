c2ec92ea21e16f0349e340c4fadb2a6a94a229d5
Mirza <mirza.ismayilov@bil.omu.edu.tr>
2023-12-14 11:54:22 +0300

    Yorum Eklendi

--- java101TeknikDersler-1/src/FibonacciSerisi.java
+++ java101TeknikDersler-1/src/FibonacciSerisi.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class FibonacciSerisi {
     public static void main(String[] args) {
+        // Kullanıcıdan alınacak sıra sayısı için bir değişken tanımlanıyor.
         int girilenSayi;
+
+        // Scanner sınıfı ile kullanıcıdan veri almak için bir nesne oluşturuluyor.
         Scanner sc = new Scanner(System.in);
+
+        // Kullanıcıdan bir sayı isteniyor.
         System.out.println("Bir sayi Gir");
         girilenSayi=sc.nextInt();
+
+        // Girilen sayıya kadar olan Fibonacci serisini hesaplayıp ekrana yazdırma işlemi
         System.out.println(fibonachi(girilenSayi));
 
     }
 
 
+    // Recursive bir şekilde Fibonacci serisini hesaplayan fonksiyon
     public static int fibonachi(int n){
+        // Base case: Eğer n 1 veya daha küçükse, 1'i geri döndür.
         if(n<=1){
             return 1;
         }else {
+            // Recursive adımlar: n-1 ve n-2 için fibonachi fonksiyonunu çağırarak topla.
             return fibonachi(n-1)+fibonachi(n-2);
         }
     }
