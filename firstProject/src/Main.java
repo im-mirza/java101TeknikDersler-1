081473bb03aa5172e3b4db7bb9ec5c374f2ac664
Mirza ismayilov <mirza.ismayilov@bil.omu.edu.tr>
2023-12-31 16:33:38 +0300

    Delete firstProject directory

--- firstProject/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // MineSweeper sınıfından bir örnek oluşturarak oyunu başlattik
-        MineSweeper mine = new MineSweeper();
-        mine.startGame();
-    }
-}
